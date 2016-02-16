038b77d9c79c727c849ddf070e9d68941f4b2ca6
Vilhjalmur Alex <vilhjalmuralex@gmail.com>
2016-02-16 11:26:11 +0000

    and some files to work with client

---
+++ js/ChatResource.js
@@ -0,0 +1,31 @@
+angular.module("chat").factory("ChatResource", function ChatResource(){
+	var socket = io.connect("http://localhost:8080");
+	/*return {
+		login: function login(user, callback){
+
+		}
+		getRoomList: function getRoomList(callback){
+
+		}
+	}*/
+	return {
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {  
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      })
+    }
+  };
+});
