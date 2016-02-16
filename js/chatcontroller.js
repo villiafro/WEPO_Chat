b0074345441ac3f7e24b4ba59cb95ee9a3f9aa50
Vilhjalmur Alex <vilhjalmuralex@gmail.com>
2016-02-16 11:25:38 +0000

    fixed the server and client

--- js/chatcontroller.js
+++ js/chatcontroller.js
@@ -3,14 +3,40 @@ angular.module("chat", ["ng", "ngRoute"]).config(function($routeProvider){
 	$routeProvider.when("/index", {
 		templateUrl: "view/home.html",
 		controller: "loginCTRL"
-	}).when("/rooms/:roomID", {
+	}).when("/roomlist", {
+		templateUrl: "view/roomlist.html",
+		controller: "roomlistCTRL"
+	}).when("/room/:roomID", {
 		templateUrl: "view/room.html",
 		controller: "chatroomCTRL"
 	}).otherwise({redirectTo: "/index"});
 });
 
-angular.module("chat").controller("loginCTRL", ["$scope", "$http", function($scope, $http){
+angular.module("chat").factory("ChatResource", function ChatResource(){
+	var socket = io.connect("http://localhost:8080");
+	return {
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {  
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      })
+    }
+  };
+});
 
+angular.module("chat").controller("loginCTRL", ["$scope", "$http", "$location", function($scope, $http, $location){
 	var socket = io.connect("http://localhost:8080");
 
 	socket.on("roomlist", function(data){
@@ -18,20 +44,29 @@ angular.module("chat").controller("loginCTRL", ["$scope", "$http", function($sco
 	});
 
 	$scope.nick = "";
-	$scope.login = function(){
+	$scope.onLogin = function(view){
 		socket.emit("adduser", $scope.nick, function(available){
 			if(available){
-				//  username is not taken!
-				socket.emit("rooms");
+				$location.path('/roomlist');
+				/*socket.emit("rooms");*/
 			}
 			else{
-				$scope.faillog = "Not available"
+				$scope.errorMessage = "FAILED!";
 			}
 		});
 
 	};
 }]);
 
-angular.module("chat").controller("chatroomCTRL", ["$scope", "$http", function($scope, $http){
+angular.module("chat").controller("chatroomCTRL", ["$scope", "$http", "ChatResource", "$location", function($scope, $http, ChatResource, $location){
 
 }]);
+
+angular.module("chat").controller("roomlistCTRL", ["$scope", "$http", "ChatResource", "$location", function($scope, $http, ChatResource, $location){
+	var socket = io.connect("http://localhost:8080");
+	$scope.rooms = "";
+	socket.emit("rooms", $scope.rooms, function(data){
+		$scope.rooms = data.name;
+	});
+}]);
+
