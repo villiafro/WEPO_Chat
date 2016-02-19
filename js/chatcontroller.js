f6b759aa1bb5a0162e8a01222de329516cbdd540
Vilhjalmur Alex <vilhjalmuralex@gmail.com>
2016-02-19 16:10:58 +0000

    threw away some trash

--- js/chatcontroller.js
+++ js/chatcontroller.js
@@ -12,30 +12,6 @@ angular.module("chat", ["ng", "ngRoute"]).config(function($routeProvider){
 	}).otherwise({redirectTo: "/index"});
 });
 
-/*angular.module("chat").factory("ChatResource", function ChatResource($rootScope){
-	var socket = io.connect("http://localhost:8080");
-	return {
-    on: function (eventName, callback) {
-      socket.on(eventName, function () {  
-        var args = arguments;
-        $rootScope.$apply(function () {
-          callback.apply(socket, args);
-        });
-      });
-    },
-    emit: function (eventName, data, callback) {
-      socket.emit(eventName, data, function () {
-        var args = arguments;
-        $rootScope.$apply(function () {
-          if (callback) {
-            callback.apply(socket, args);
-          }
-        });
-      })
-    }
-  };
-});*/
-
 angular.module("chat").controller("loginCTRL", ["$scope", "$http", "$location", function($scope, $http, $location){
 	var socket = io.connect("http://localhost:8080");
 
